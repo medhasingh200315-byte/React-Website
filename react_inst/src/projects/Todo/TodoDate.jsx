7d1dfb3281a810d80d5f8faab8e7924714609a7b
Medha Singh <medhasingh200315@gmail.com>
2025-09-25 13:26:45 +0530

    TodoDate.jsx

--- react_inst/src/projects/Todo/TodoDate.jsx
+++ react_inst/src/projects/Todo/TodoDate.jsx
@@ -21,25 +21,3 @@ export const TodoDate=()=>{
     return <h2 className="date-time">{dateTime}</h2>;
 };
 
-//................................................PRACTISE.....................................
-
-// export const TodoDate=()=>{
-
-//     const[dateTime,setDateTime]=useState("");
-//     useEffect(()=>{
-//     const getDateTime=()=>{
-//     const now=new Date();
-//     const formattedDate=now.toLocaleDateString();
-//     const formattedTime=now.toLocaleTimeString();
-
-//     setDateTime(`${formattedDate}-${formattedTime}`);
-//     }
-
-//     const interval=setInterval(()=>{getDateTime();},1000)
-
-//     return()=>clearInterval(interval)
-
-//     },[])
-
-//     return <h2>{dateTime}</h2>
-// } 
